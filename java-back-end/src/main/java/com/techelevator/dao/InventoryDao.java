06bb71bece2038ef55f471a83b3edc650dffbdaa
ForrestYankey <forrestyankey@gmail.com>
2024-01-01 14:42:44 -0500

    create InventoryDao

--- java-back-end/src/main/java/com/techelevator/dao/InventoryDao.java
+++ java-back-end/src/main/java/com/techelevator/dao/InventoryDao.java
@@ -1,6 +1,13 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.InventoryItem;
+
+import java.util.List;
+
 public interface InventoryDao {
 
-
+    InventoryItem addNewInventoryItem();
+    List<InventoryItem> fetchInventoryItems();
+    InventoryItem updateInventoryItem();
+    boolean deleteInventoryItem();
 }
